a0d7c2a6acd1fa810a9df0392419d91b345cf20f
Mustafa Akcakaya <m.akcakaya.tr@gmail.com>
2026-09-13 00:14:17 +0300

    Add YARP rate limiting and Docker routing

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,10 +1,23 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("fixed", limiterOptions =>
+    {
+        limiterOptions.PermitLimit = 5;
+        limiterOptions.Window = TimeSpan.FromSeconds(10);
+    });
+});
+
 var app = builder.Build();
+
+app.UseRateLimiter();
 
 app.MapReverseProxy();
 
